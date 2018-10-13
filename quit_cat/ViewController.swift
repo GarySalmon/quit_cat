b8e2cda8af5e9413c986e6090629c82616a38762
Gary <Gary@garydemacbook-pro.local>
2018-10-14 01:27:44 +0800

    Initial Commit

---
+++ quit_cat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quit_cat
+//
+//  Created by Gary on 2018/10/14.
+//  Copyright © 2018 Gary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
